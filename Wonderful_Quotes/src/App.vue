9d7a376ed860905d2924157d96b6fcf6585807ca
dounisca <dounisca@yahoo.com>
2020-05-26 18:16:19 +0100

    Adding New Quotes

--- Wonderful_Quotes/src/App.vue
+++ Wonderful_Quotes/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <app-New-Quote @quoteAdded="newQuote"></app-New-Quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./Components/QuoteGrid";
+import newQuote from "./Components/NewQuote";
 
 export default {
   data: function(){
@@ -16,9 +18,15 @@ export default {
       maxQuotes: 10
     }
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
   components: {
-    appQuoteGrid: QuoteGrid
-  }
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: newQuote
+  },
 }
 </script>
 
