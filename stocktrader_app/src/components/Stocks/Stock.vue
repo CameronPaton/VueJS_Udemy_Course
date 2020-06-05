861e5a46369c5f52af0630a24bdd430dc209567c
dounisca <dounisca@yahoo.com>
2020-06-05 12:11:20 +0100

    Updated the Routes

--- stocktrader_app/src/components/Stocks/Stock.vue
+++ stocktrader_app/src/components/Stocks/Stock.vue
@@ -3,13 +3,13 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
-          Stock Name
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{stock.price}})</small>
         </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
-          <input type="number" class="form-control" placeholder="Quantity" />
+          <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" />
         </div>
         <div class="pull-right">
           <button class="btn btn-success">Buy</button>
@@ -20,5 +20,12 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  }
+};
 </script>
